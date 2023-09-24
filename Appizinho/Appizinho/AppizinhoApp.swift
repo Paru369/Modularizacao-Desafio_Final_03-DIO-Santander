ca391642ce19b3b4470f8cfe02625f5117773b5f
parutech <paruvitu@gmail.com>
2023-09-24 11:46:12 -0300

    add files

---
+++ Appizinho/Appizinho/AppizinhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppizinhoApp.swift
+//  Appizinho
+//
+//  Created by Paulo Pinheiro on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct AppizinhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
